2471310ab0f60544562393f8be850296287bc6dd
Aleksandr Shakhoff <sas7289@yandex.ru>
2021-06-29 00:48:48 +0300

    added check password

--- src/main/java/com/example/my_market/frontend/RegistrationView.java
+++ src/main/java/com/example/my_market/frontend/RegistrationView.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -28,19 +29,23 @@ public class RegistrationView extends VerticalLayout {
     private final UserService userService;
     private final Map<String, AbstractField> textFields;
     private final RoleService roleService;
+    private final Label topLabel;
 
     {
+        topLabel = new Label();
+        topLabel.setVisible(false);
         setAlignItems(Alignment.CENTER);
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         PasswordField f = new PasswordField();
         textFields = new LinkedHashMap<>();
-        textFields.put("Login", new TextField("Login"));
-        textFields.put("Password", new PasswordField("Password"));
-        textFields.put("Name", new TextField("name"));
-        textFields.put("Middle_name", new TextField("middle_name"));
-        textFields.put("Last_name", new TextField("last_name"));
-        textFields.put("Phone", new TextField("phone"));
+        textFields.put("Login", new TextField("Логин"));
+        textFields.put("Password", new PasswordField("Пароль"));
+        textFields.put("RePassword", new PasswordField("Пароль (ещё раз)"));
+        textFields.put("Last_name", new TextField("Фамилия"));
+        textFields.put("Name", new TextField("Имя"));
+        textFields.put("Middle_name", new TextField("Отчество"));
+        textFields.put("Phone", new TextField("Телефон"));
     }
 
     public RegistrationView(UserService userService, RoleService roleService) {
@@ -60,26 +65,32 @@ public class RegistrationView extends VerticalLayout {
 //        phone.setLabel("phone");
         Button button = new Button("Зарегистрироваться");
         button.addClickListener(event -> {
-            if(!checkNotNull()) {
+            if (!checkNotNull()) {
                 return;
             }
             User newUser = new User();
             Role role = roleService.getRole("user");
             newUser.setId(UUID.randomUUID());
-            newUser.setLogin(((TextField)textFields.get("Login")).getValue());
+            newUser.setLogin(((TextField) textFields.get("Login")).getValue());
 //            newUser.setPassword(textFields.get("Password").getValue());
+            if (!((PasswordField) textFields.get("Password")).getValue().equals(((PasswordField) textFields.get("RePassword")).getValue())) {
+                topLabel.setVisible(true);
+                topLabel.setText("Пароли не совпадают!");
+                return;
+            }
+            topLabel.setVisible(false);
             newUser.setPassword(BCrypt.hashpw(((PasswordField) textFields.get("Password")).getValue(), BCrypt.gensalt()));
-            newUser.setName(((TextField)textFields.get("Name")).getValue());
+            newUser.setName(((TextField) textFields.get("Name")).getValue());
 
-            newUser.setMiddle_name(((TextField)textFields.get("Middle_name")).getValue());
-            newUser.setLast_name(((TextField)textFields.get("Last_name")).getValue());
-            newUser.setPhone(((TextField)textFields.get("Phone")).getValue());
+            newUser.setMiddle_name(((TextField) textFields.get("Middle_name")).getValue());
+            newUser.setLast_name(((TextField) textFields.get("Last_name")).getValue());
+            newUser.setPhone(((TextField) textFields.get("Phone")).getValue());
             newUser.setRole(role);
 //            newUser.setRole(new Role("user"));
             userService.saveUser(newUser);
             UI.getCurrent().navigate("login");
         });
-
+        add(topLabel);
         add(textFields.values().toArray(new AbstractField[6]));
         add(button);
     }
@@ -103,7 +114,7 @@ public class RegistrationView extends VerticalLayout {
 
     private boolean checkNotNull() {
         for (AbstractField field : textFields.values()) {
-            if(field.isEmpty()) {
+            if (field.isEmpty()) {
                 return false;
             }
         }
